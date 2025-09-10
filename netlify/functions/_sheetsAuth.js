a3c487a08043f9530b4373e9d0aa6b88ec8c4299
yatajwale7777 <yatajwale7777@gmail.com>
2025-09-10 13:51:16 +0530

    Fix: export getSheetsClientFromBase64 from _sheetsAuth

--- netlify/functions/_sheetsAuth.js
+++ netlify/functions/_sheetsAuth.js
@@ -1,9 +1,22 @@
-const { google } = require("googleapis");
-const path = require("path");
+// netlify/functions/_sheetsAuth.js
+const { google } = require('googleapis');
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, "../../key.json"), // adjust path as needed
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
+function getSheetsClientFromBase64(base64json) {
+  if (!base64json) throw new Error('GOOGLE_CREDENTIALS_BASE64 not set');
+  // decode base64 -> JSON
+  let key;
+  try {
+    key = JSON.parse(Buffer.from(base64json, 'base64').toString('utf8'));
+  } catch (err) {
+    throw new Error('Failed to parse base64 JSON: ' + err.message);
+  }
+  const auth = new google.auth.GoogleAuth({
+    credentials: key,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  });
+  const sheets = google.sheets({ version: 'v4', auth });
+  return sheets;
+}
 
-module.exports = auth;
+module.exports = { getSheetsClientFromBase64 };
+
